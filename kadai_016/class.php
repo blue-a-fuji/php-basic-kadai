5530b6f739b52b8b8d7762cd158ce43668bc74a7
blue-a-fuji <chocolate.akko54@gmail.com>
2025-05-11 16:39:36 +0900

    kadai_016フォルダのアップロード

---
+++ kadai_016/class.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>課題016</title>
+</head>
+
+<body>
+   <p>
+       <?php
+       // クラスを定義する
+       class Food {
+          // プロパティを定義する
+          private $name;
+          private $price;
+
+          //  コンストラクタを定義する
+          public function __construct(string $name, int $price) {
+            $this->name = $name;
+            $this->price = $price;
+          }
+
+          //  メソッドを定義する
+          public function show_price() {
+            echo $this->price . '<br>';
+          }
+       }
+
+       // インスタンス化する
+       $food = new Food('potate', 250);
+
+       //  インスタンス$foodの各プロパティの値を出力する
+       print_r($food);      
+       ?>
+   </p>
+
+   <p>
+       <?php
+       // クラスを定義する
+       class Animal {
+          // プロパティを定義する
+          private $name;
+          private $height;
+          private $weight;
+
+          //  コンストラクタを定義する
+          public function __construct(string $name, int $height, int $weight) {
+            $this->name = $name;
+            $this->height = $height;
+            $this->weight = $weight;
+          }
+
+          //  メソッドを定義する
+          public function show_height() {
+            echo $this->height. '<br>';
+          }
+
+       }
+
+       // インスタンス化する
+       $animal = new Animal('dog', 60, 5000);
+
+       //  インスタンス$foodの各プロパティの値を出力する
+       print_r($animal);   
+       ?>
+   </p>   
+</body>
+
+</html>
